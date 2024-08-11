d13ee313434aebd31fd12f5dbf5f0013e50d3b78
Taw-Ham Almeida Balbino de Paula <taw.ham.balbino9914@gmail.com>
2024-08-10 21:04:44 -0300

    atividade concluída

--- src/main/java/com/gugawag/pdist/ejbs/UsuarioService.java
+++ src/main/java/com/gugawag/pdist/ejbs/UsuarioService.java
@@ -6,6 +6,7 @@ import com.gugawag.pdist.model.Usuario;
 import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import java.util.Arrays;
 import java.util.List;
 
 @Stateless(name = "usuarioService")
@@ -17,17 +18,28 @@ public class UsuarioService {
 
     @EJB
     private MensagemDAO mensagemDao;
+    private static final List<String> PALAVROES = Arrays.asList("bicha", "buceta", "bosta", "cagar", "caralho", "cu", "foda-se", "foder", "porra", "piroca", "puta", "merda", "viado");
+
 
     public List<Usuario> listar() {
         return usuarioDao.listar();
     }
 
-    public void inserir(long id, String nome) {
+    public List<Mensagem> listarMensagens() {
+        return mensagemDao.listar();
+    }
+
+    public void inserir(long id, String nome, String mensagem) {
         Usuario novoUsuario = new Usuario(id, nome);
         usuarioDao.inserir(novoUsuario);
 
-        Mensagem mensagem = new Mensagem();
-//        mensagemDao.inserir(mensagem);
+        Mensagem mensagemBD = new Mensagem(id, mensagem);
+        mensagemDao.inserir(mensagemBD);
+        for (String palavrao: PALAVROES) {
+            if (mensagem.toLowerCase().contains(palavrao)) {
+                throw new RuntimeException("Não pode contr palavrões na mensagem");
+            }
+        }
         if (id==3L) {
             throw new RuntimeException("Menor de idade não permitido!");
         }
--- src/main/java/com/gugawag/pdist/servlets/UsuarioServlet.java
+++ src/main/java/com/gugawag/pdist/servlets/UsuarioServlet.java
@@ -1,12 +1,14 @@
 package com.gugawag.pdist.servlets;
 
 import com.gugawag.pdist.ejbs.UsuarioService;
+import com.gugawag.pdist.model.Mensagem;
 import com.gugawag.pdist.model.Usuario;
 
 import javax.ejb.EJB;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/usuario.do"})
 public class UsuarioServlet extends javax.servlet.http.HttpServlet {
@@ -22,15 +24,18 @@ public class UsuarioServlet extends javax.servlet.http.HttpServlet {
             case "1": {
                 long id = Integer.parseInt(request.getParameter("id"));
                 String nome = request.getParameter("nome");
-                usuarioService.inserir(id, nome);
+                String mensagem = request.getParameter("mensagem");
+                usuarioService.inserir(id, nome, mensagem);
             }
             case "2": {
+                int i = 0;
+                List<Mensagem> mensagens = usuarioService.listarMensagens();
                 for(Usuario usuario: usuarioService.listar()){
-                    resposta.println(usuario.getNome());
+                    resposta.println(usuario.getNome() + " - " + mensagens.get(i).getMensagem());
+                    i++;
                 }
                 break;
             }
-
         }
     }
 }
